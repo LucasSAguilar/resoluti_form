c7ee0fbd88bae2faf970ee7cf200d0734ca14c0b
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-01-16 15:27:21 -0300

    novas telas e design

--- src/componentes/Inputs/index.js
+++ src/componentes/Inputs/index.js
@@ -1,10 +1,24 @@
 import styles from "./inputs.module.css";
 
-export default function Inputs ({label, placeholder, value, type = "text", estilosInput, estilosContainer}){
-    return (
-        <div className={`${styles.containerInput} ${estilosContainer}`}>
-            <label className={styles.label}>{label}</label>
-            <input type={type} className={`${styles.input} ${estilosInput}`} placeholder={placeholder} value={value}/>
-        </div>
-    )
+export default function Inputs({
+  label,
+  placeholder,
+  value,
+  type = "text",
+  estilosInput,
+  estilosContainer,
+  aoAlterado,
+}) {
+  return (
+    <div className={`${styles.containerInput} ${estilosContainer}`}>
+      <label className={styles.label}>{label}</label>
+      <input
+        onChange={(e) => aoAlterado(e.target.value)}
+        type={type}
+        className={`${styles.input} ${estilosInput}`}
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  );
 }
--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -4,9 +4,13 @@ import Inputs from "componentes/Inputs";
 import Botao from "componentes/Botao";
 import Logo from "img/logoResoluti.webp";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Cadastro() {
   const navigate = useNavigate();
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <Tela>
@@ -18,17 +22,20 @@ export default function Cadastro() {
             estilosContainer={styles.estilosContainer}
             label={"Username"}
             placeholder={"Insira seu username"}
+            aoAlterado={(conteudo) => setUserName(conteudo)}
           />
           <Inputs
             estilosContainer={styles.estilosContainer}
             label={"Senha"}
             placeholder={"Insira sua senha"}
+            aoAlterado={(conteudo) => setPassword(conteudo)}
+            type="password"
           />
           <p className={styles.alerta}>Usuário não encontrado</p>
           <Botao
             value={"Entrar"}
             onClick={() => {
-              navigate("/formulario");
+                //navigate("/formulario");}
             }}
           />
         </div>
